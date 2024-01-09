46104b19aea1bff5e82c95c94e1ac93db2d7cd8a
DanielZh <haveacar.zhovnir@gmail.com>
2024-01-09 15:51:07 +0200

    fix main.py/002

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ def run():
     # get config from settings file
     configuration = upload_configuration()
 
-    car_count = CarCount(configuration.get('cascade_name'))
-    car_count.generate_frames(configuration.get('video_link'))
+    car_count = CarCount(configuration.get('cascade_name'),
+                         configuration.get('db_name'),
+                         configuration.get('db_user'),
+                         configuration.get('db_pass'),
+                         configuration.get('db_host'),
+                         )
+
+
+    car_count.generate_frames(
+        configuration.get('video_link'))
 
 
 if __name__ == '__main__':
